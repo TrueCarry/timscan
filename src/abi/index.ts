330468c0eb4a8a21d7434cd09c73d6f4ea83732e
Tim <truecarry@gmail.com>
2022-07-20 01:28:46 +0300

    Differennt import

--- src/abi/index.ts
+++ src/abi/index.ts
@@ -14,11 +14,18 @@ export interface OutputArg {
 }
 
 export const abiMap: Record<string, Promise<ContractAbi>> = {
-  '4c9123828682fa6f43797ab41732bca890cae01766e0674100250516e0bf8d42': importPath('./nft'), // standard nft
-  '9892766765d3ea42809a417abbd7ff9ce681b145d05ae6b118a614b38c8ded15': importPath('./nft'), // standard editable nft
-  feb5ff6820e2ff0d9483e7e0d62c817d846789fb4ae580c878866d959dabd5c0: importPath('./wallet'), // standard editable nft,
-  '9494d1cc8edf12f05671a1a9ba09921096eb50811e1924ec65c3c629fbb80812':
-    importPath('./highloadWallet'), // standard editable nft,
+  '4c9123828682fa6f43797ab41732bca890cae01766e0674100250516e0bf8d42': import('./nft').then(
+    (i) => i.default
+  ), // standard nft
+  '9892766765d3ea42809a417abbd7ff9ce681b145d05ae6b118a614b38c8ded15': import('./nft').then(
+    (i) => i.default
+  ), // standard editable nft
+  feb5ff6820e2ff0d9483e7e0d62c817d846789fb4ae580c878866d959dabd5c0: import('./wallet').then(
+    (i) => i.default
+  ), // standard editable nft,
+  '9494d1cc8edf12f05671a1a9ba09921096eb50811e1924ec65c3c629fbb80812': import(
+    './highloadWallet'
+  ).then((i) => i.default), // standard editable nft,
 }
 
 function importPath(src: string) {
