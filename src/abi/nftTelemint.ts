43344f57985dcb81332df25537a8f5fbea80fce5
Tim <truecarry@gmail.com>
2023-01-18 20:34:17 +0300

    Add teleming auction state

--- src/abi/nftTelemint.ts
+++ src/abi/nftTelemint.ts
@@ -17,6 +17,29 @@ const telemintNftAbi: ContractAbi = {
         },
       ],
     },
+
+    get_telemint_auction_state: {
+      input: [],
+      output: [
+        { name: 'bidder_address', type: 'address' },
+        { name: 'bid', type: 'uint' },
+        { name: 'bid_ts', type: 'uint' },
+        { name: 'min_bid', type: 'uint' },
+        { name: 'end_time', type: 'uint' },
+      ],
+    },
+
+    get_telemint_auction_config: {
+      input: [],
+      output: [
+        { name: 'beneficiary_address', type: 'address' },
+        { name: 'initial_min_bid', type: 'uint' },
+        { name: 'max_bid', type: 'uint' },
+        { name: 'min_bid_step', type: 'uint' },
+        { name: 'min_extend_time', type: 'uint' },
+        { name: 'duration', type: 'uint' },
+      ],
+    },
   },
 }
 
